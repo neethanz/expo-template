cb3eee9b4e8397acfd0561c024e4a314de915ae0
Nz <Kalaineethan@gmail.com>
2025-02-03 11:45:12 +0530

    init appwrite

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,21 @@
 import { Text, View } from "react-native";
+import "react-native-url-polyfill/auto";
+import { Account, ID } from "react-native-appwrite";
+import client from "../appwrite/config";
+// Init your React Native SDK
+// Your application ID or bundle ID.
+const account = new Account(client);
 
 export default function Index() {
+  const createUser = async () => {
+    const user = await account.create(
+      ID.unique(),
+      "test@test.com",
+      "password",
+      "test"
+    );
+    console.log(user);
+  };
   return (
     <View
       style={{
---
+++ appwrite/config.js
@@ -0,0 +1,7 @@
+import { Client } from "react-native-appwrite";
+
+const client = new Client()
+  .setProject("67a059f6000f1fade659")
+  .setPlatform("com.kalaineethanarmia.expotemplate");
+
+export default client;
